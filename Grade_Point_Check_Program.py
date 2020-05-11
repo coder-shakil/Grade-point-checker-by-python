3827b2feef87bc98f66b0518f9f5ce009f29ac14
Sakil Mahmud <38534880+codersakil@users.noreply.github.com>
2020-05-12 04:39:40 +0600

    Add files via upload

---
+++ Grade_Point_Check_Program.py
@@ -0,0 +1,25 @@
+mark = int(input("Enter Your Exam Number :"))
+
+if mark >= 80:
+    print("A+")
+
+elif mark >= 70:
+    print("A")
+
+elif mark >= 60:
+    print("A-")
+
+elif mark >= 50:
+    print("B")
+
+elif mark >= 40:
+    print("Your Exam Grade Is :C")
+
+elif mark >= 33:
+    print("D")
+
+
+else:
+    print("Sorry.You Failed")
+
+print("Thank You")
